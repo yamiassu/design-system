c865585aa4fe3e6dead2aebf64e1bc401841c2e2
Rafael Correa <rafael.chaves98@hotmail.com>
2021-10-24 07:27:10 -0300

    enhanced display capabilities

--- lib/js/components/Display/props.ts
+++ lib/js/components/Display/props.ts
@@ -1,14 +1,5 @@
-// Packages
-import type { VariantProps } from "@stitches/react"
-
-// Component
-import { textCss } from "./style"
-
-export default interface PropsInterface extends VariantProps<typeof textCss> {
+export default interface PropsInterface {
 	// required
 	children?: string | any;
 	data?: string | any;
-
-	// Optional
-	required?: boolean;
 }
